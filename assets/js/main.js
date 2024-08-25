25a923d5b7c8dd33e11940b6fdfc7715b838da90
sahilsaini5 <85404123+sahilsaini5@users.noreply.github.com>
2024-08-25 23:26:26 +0530

    typed.js library

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,47 @@
+// const homeName = new Typed('.home__name', {
+//   strings: ['Sahil ^1000 Saini_'],
+//   typeSpeed: 200,
+//   cursorChar: '',
+// });
+
+const homeWork = new Typed('.home__work', {
+  strings: ['Full Stack | Blockchain | Freelancer'],
+  typeSpeed: 50,
+  cursorChar: '',
+});
+
+
+
+
 /*=============== Active Link =============== */
-const navlink = document.querySelectorAll(".nav__link");
+// const navlink = document.querySelectorAll(".nav__link");
 
-function activeLink() {
-  navlink.forEach((n) => n.classList.remove("active-link"));
-  this.classList.add("active-link");
-}
+// function activeLink() {
+//   navlink.forEach((n) => n.classList.remove("active-link"));
+//   this.classList.add("active-link");
+// }
 
-navlink.forEach((n) => n.addEventListener("click", activeLink));
+// navlink.forEach((n) => n.addEventListener("click", activeLink));
 
 /*=============== Background Header =============== */
-function scrollHeader() {
-  const header = document.getElementById("header");
+// function scrollHeader() {
+//   const header = document.getElementById("header");
 
-  if (this.scrollY >= 50) header.classList.add("scroll-header");
-  else header.classList.remove("scroll-header");
-}
+//   if (this.scrollY >= 50) header.classList.add("scroll-header");
+//   else header.classList.remove("scroll-header");
+// }
 
 // window.addEventListener("scroll", scrollHeader);
 
 /*=============== Active Work =============== */
-const linkWork = document.querySelectorAll(".category__btn");
+// const linkWork = document.querySelectorAll(".category__btn");
 
-function activeWork() {
-  linkWork.forEach((n) => n.classList.remove("active-work"));
-  this.classList.add("active-work");
-}
+// function activeWork() {
+//   linkWork.forEach((n) => n.classList.remove("active-work"));
+//   this.classList.add("active-work");
+// }
 
-linkWork.forEach((n) => n.addEventListener("click", activeWork));
+// linkWork.forEach((n) => n.addEventListener("click", activeWork));
 
 /*=============== Mixitup Filter =============== */
 // let mixerProjects = mixitup(".projects__container", {
@@ -39,41 +54,41 @@ linkWork.forEach((n) => n.addEventListener("click", activeWork));
 // });
 
 /*=============== Testimonials Swiper =============== */
-var testSwiper = new Swiper(".testimonial__container", {
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
-});
+// var testSwiper = new Swiper(".testimonial__container", {
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   mousewheel: true,
+//   keyboard: true,
+// });
 
 /*=============== Email Js =============== */
-const contactForm = document.getElementById("contact__form"),
-  contactName = document.getElementById("contact-name"),
-  contactEmail = document.getElementById("contact-email"),
-  Message = document.getElementById("message"),
-  contactMessage = document.getElementById("contact-message");
+// const contactForm = document.getElementById("contact__form"),
+//   contactName = document.getElementById("contact-name"),
+//   contactEmail = document.getElementById("contact-email"),
+//   Message = document.getElementById("message"),
+//   contactMessage = document.getElementById("contact-message");
 
-const sendEmail = (e) => {
-  e.preventDefault();
+// const sendEmail = (e) => {
+//   e.preventDefault();
 
-  if (
-    contactName.value === "" ||
-    contactEmail.value === "" ||
-    Message.value === ""
-  ) {
-    contactMessage.classList.remove("color-light");
-    contactMessage.classList.add("color-dark");
+//   if (
+//     contactName.value === "" ||
+//     contactEmail.value === "" ||
+//     Message.value === ""
+//   ) {
+//     contactMessage.classList.remove("color-light");
+//     contactMessage.classList.add("color-dark");
 
-    contactMessage.textContent = "Write all the fields";
-  } else {
-    emailjs.sendForm("", "", "", "");
-  }
-};
+//     contactMessage.textContent = "Write all the fields";
+//   } else {
+//     emailjs.sendForm("", "", "", "");
+//   }
+// };
 
 // contactForm.addEventListener("submit", sendEmail);
