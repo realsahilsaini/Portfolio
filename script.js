be196135628463f03d96adbd4fa0d65c22de6316
sahilsaini5 <85404123+sahilsaini5@users.noreply.github.com>
2024-09-09 21:37:45 +0530

    Portfolio revamp

---
+++ script.js
@@ -0,0 +1,16 @@
+// const hero_data = document.querySelector('.hero-data');
+// hero_data.style.setProperty('--animate-duration', '1s');
+
+// const element = document.querySelector('.home-socials');
+// element.style.setProperty('--animate-duration', '2s');
+
+
+var typed = new Typed('#typed-js', {
+  strings: ['Full Stack', 'Dapps / Web3', 'Data Engineering', 'MLOps', 'Freelancer'],
+  startDelay: 0,
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+  showCursor: true,
+  contentType: 'html',
+});
